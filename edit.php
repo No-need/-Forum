1a0779f3abce39871fcef0d26f2edfb439374900
No-need <39295066+No-need@users.noreply.github.com>
2019-10-03 17:53:04 +0800

    2019/10/3
    編輯留言

--- edit.php
+++ edit.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>編輯</title>
     <?php
-    //$id = $_GET['id'];
-    $id =1;
+    $id = $_GET['id'];
+    
     $mysqli = new mysqli("localhost","root","");
     $mysqli->select_db("work");
     $mysqli->query('SET NAMES utf8');
@@ -15,19 +15,22 @@
     $row = mysqli_fetch_row($result);
     echo $row[0].$row[1].$row[2];
     if(isset($_POST["title"])){
-       
+        
         $title = $_POST['title'];
         $content = $_POST['content'];
-        $updated_at =date('Y-m-d H:i:s');
-        $sql ="update message set title='$title,content=$content,updated_at=$updated_at, where id='$id'";
+        $updated_at =date('Y-m-d H:i:s',strtotime('+6HOURS'));
+        $sql ="UPDATE posts SET title=?,content=?,updated_at=? where id=?";
         $stmt = $mysqli->prepare($sql);
-        
+        $stmt->bind_param('sssi',$title,$content,$updated_at,$id);
         $stmt->execute();
+        echo $updated_at;
+        header('location:main.html');
         }
+    
     ?>
 </head>
 <body>
-<form action="edit.php" method="post">
+<form action="edit.php?id=<?php echo $id ?>" method="post">
 <table border="1">
   <tr>
     <td><font size="2">標題:</font></td>
